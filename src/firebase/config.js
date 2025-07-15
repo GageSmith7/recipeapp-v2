531738ec151b9686717f46a1825497064742b431
GageSmith7 <158121485+GageSmith7@users.noreply.github.com>
2025-07-15 12:47:30 -0700

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,9 @@
-// firebase/config.js - Updated with Firebase Storage
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
